f635c4d78af83c29aea354bcf163950978579b8f
OkiStuff <40648091+OkiStuff@users.noreply.github.com>
2020-04-21 09:19:46 -0400

    Main code and bells.txt added to Bell Tracker

---
+++ main1.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return render_template("index.html")
+
+
+@app.route("/bells/<bellsINPUT>")
+def bellsSave(bellsINPUT):
+
+    
+    bellsFile = open("bells.txt", "r")
+    isEmpty = bellsFile.read()
+
+    if isEmpty == "this broke so yeah :(":
+
+        bellsFile = open("bells.txt", "w")
+        bellsFile.write(bellsINPUT)
+
+        bellsFile.close()
+
+    else:
+
+        
+
+        bellsFile = open("bells.txt", "w")
+
+        bellsBefore = int(isEmpty)
+
+        bellsCurrent = int(bellsINPUT)
+
+        bellsAfter = bellsBefore + bellsCurrent
+        bellsSum = str(bellsAfter)
+
+        bellsFile.write(bellsSum)
+        bellsFile.close()
+
+    return render_template("saved.html")
+
+if __name__ == "__main__":
+    app.run()
